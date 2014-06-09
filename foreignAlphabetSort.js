5246fea539ee5350654e878a57262d18ee7ab714
willwclo <willwclo@gmail.com>
2014-06-08 22:50:42 -0700

    Formatting notes

--- foreignAlphabetSort.js
+++ foreignAlphabetSort.js
@@ -27,6 +27,7 @@ var foreignAlphabetSort = function(inputArray) {
     return words.sort(sortFunc);
 };
 
+
 /* 
 Description:
 The Gorellians, at the far end of our galaxy, have discovered various samples of English text from our electronic transmissions, but they did not find the order of our alphabet. Being a very organized and orderly species, they want to have a way of ordering words, even in the strange symbols of English. Hence they must determine their own order.
@@ -41,6 +42,7 @@ HILL
 ANY
 ANTLER
 COW
+
 Input:
 The input will be formatted to enter the number of words to sort and the new Alphabet ordering and a list of words to sort. n should be > 0. The alphabet is assumed to be 26 letters with no duplicates and arranged in the new order. Also assumed there are n strings entered.
 n (new alphabet ordering)
@@ -48,6 +50,7 @@ n (new alphabet ordering)
 (word 2 of n)
 ....
 (word n of n)
+
 Example input 1:
 8 UVWXYZNOPQRSTHIJKLMABCDEFG
 ANTLER
@@ -58,8 +61,10 @@ HOW
 HOWEVER
 WHATEVER
 ZONE
+
 Output:
 The list of words in sorted order based on the new order of the alphabet. The sort order should be based on the alphabet (case insensitive) and the words should be output to appear as the words were entered.
+
 Example of output for input 1:
 WHATEVER
 ZONE
@@ -69,8 +74,10 @@ HILL
 ANY
 ANTLER
 COW
+
 Notes:
 The sorting should be case insensitive. Meaning that you do not sort it based on the ASCII value of the letters but by the letters. Your solution should handle an alphabet order that might be typed in upper/lower case. It will sort the words by this order and output the words as they were typed in.
+
 Example Input 2:
 5 ZYXWVuTSRQpONMLkJIHGFEDCBa
 go
