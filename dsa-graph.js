4f937dbcc8f0111626cb77802f2a24a5643fbcea
willwclo <willwclo@gmail.com>
2014-07-17 23:26:21 -0700

    Graphs

--- dsa-graph.js
+++ dsa-graph.js
@@ -7,10 +7,36 @@ Graph.prototype.addEdge = function (node) {
     this.edges.push(node);
 };
 
-Graph.prototype.depthFirstSearch = function (callback) {
-    
+Graph.prototype.depthFirstSearch = function (visitCallback) {
+    var pointers = [];
+    var traverse = function (node) {
+        visitCallback(node);
+        pointers.push(node);
+        node._discovered = true;
+        for (var i = 0; i < node.edges.length; i++) {
+            if (!node.edges[i]._discovered) { node.edges[i].depthFirstSearch(visitCallback); }
+        }
+    };
+    traverse(this);
+
+    for (var i = 0; i < pointers.length; i++) { delete pointers[i]._discovered; }
 };
 
-Graph.prototype.breadthFirstSearch = function (callback) {
+Graph.prototype.breadthFirstSearch = function (visitCallback) {
+    var pointers = [];
 
+    var queue = [];
+    queue.shift(this);
+    while (queue.length > 0) {
+        var node = queue.pop();
+        pointers.push(node);
+        visitCallback(node);
+        node._discovered = true;
+        for (var i = 0; i < node.edges.length; i++) {
+            if (!this.edges[i]._discovered) { queue.shift(node.edges[i]); }
+        }
+    }
+
+    // clean up pointers
+    for (var i = 0; i < pointers.length; i++) { delete pointers[i]._discovered; }
 };
