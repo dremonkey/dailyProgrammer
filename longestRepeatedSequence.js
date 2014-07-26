d2252733a99351ecd53dc810b96d1cf24aaea59d
dremonkey <a.deutmeyer@gmail.com>
2014-07-26 12:44:15 -0700

    added solutions for longestRepeatedSequence and spiralTraversal

--- longestRepeatedSequence.js
+++ longestRepeatedSequence.js
@@ -1,3 +1,39 @@
 // Given a string, find the start position of the largest block of repeated characters.
 // If equal in length, return the first substring
 // E.g. f(aaaabbbb) = '0' // not 4
+
+var longestRepeatedString = function (string) {
+  var startPos = 0;
+  var i = 0;
+  var maxCount = 0;
+  var tmpPos = null;
+  var tmpCounter = 1;
+
+  // loop through the string
+  while (i < string.length) {
+    var current = string[i];
+    var next = string[i+1];
+
+    // check the next index to see if it is the same as the current
+    // if it is the same, start a counter and save the current index
+    if (current === next) {
+      tmpCounter++;
+      if (tmpPos === null) tmpPos = i;
+    } else {
+      // check the current count against the maxcount
+      // if it is more than the maxcount, set maxcount equal to count and save the index
+      if (tmpCounter > maxCount) {
+        maxCount = tmpCounter;
+        startPos = tmpPos;
+      }
+
+      // reset the tmpCounter and tmpPost
+      tmpCounter = 1;
+      tmpPos = null;
+    }
+
+    i++;
+  }
+
+  return startPos;
+};
--- spiralTraversal.js
+++ spiralTraversal.js
@@ -1,7 +1,88 @@
 /* do a spiral traverse of an NxN matrix and output a flat array
  * for example given a matrix that looks like this:
  [1 3 6]
- [9 3 5]
+ [9 7 5]
  [2 8 4]
- * your output should look like this: [1 3 6 5 4 8 2 9 3]
+ * your output should look like this: [1 3 6 5 4 8 2 9 7]
  */
+
+var spiralTraversal = function (matrix) {
+  var output = [];
+  var startRow = 0;
+  var endRow = matrix.length - 1;
+  var startCol = 0;
+  var endCol = matrix[0].length - 1;
+  var len = matrix[0].length * matrix.length;
+
+  var traverse = function (rowOrCol, start, end) {
+    var i;
+
+    // if going backwards
+    if (start > end) {
+      for (i = start; i >= end; i--) {
+        output.push(rowOrCol[i]);
+      }
+    } else {
+      for (i = start; i <= end; i++) {
+        output.push(rowOrCol[i]);
+      }
+    }
+  };
+
+  var getCol = function (colNum) {
+    var col = [];
+    
+    for (var i = 0; i < matrix.length; i++) {
+      col.push(matrix[i][colNum]);
+    }
+
+    return col;
+  };
+
+
+  var nextCol = endCol;
+  var nextRow = startRow;
+
+  while (output.length < len) {
+    // left to right
+    if (startRow === nextRow) {
+      traverse(matrix[startRow], startCol, endCol);
+      startRow++;
+      nextCol = endCol;
+    }
+    // top to bottom
+    else if (endCol === nextCol) {
+      traverse(getCol(endCol), startRow, endRow);
+      endCol--;
+      nextRow = endRow;
+    }
+    // right to left
+    else if (endRow === nextRow) {
+      traverse(matrix[endRow], endCol, startCol);
+      endRow--;
+      nextCol = startCol;
+    }
+    // bottom to top
+    else if (startCol === nextCol) {
+      traverse(getCol(startCol), endRow, startRow);
+      startCol++;
+      nextRow = startRow;
+    }
+  }
+
+  return output;
+
+  // loop through the cols
+  // when you have hit max cols, increment start row and loop through the rows
+  // when you have hit max rows, decrement last col and loop through the cols backwards
+  // when you get to the first column, decrement last row and loop through the rows backwards
+  // repeat from step 1, beginning at the start row
+};
+
+var matrix = [
+   [1, 3, 6],
+   [9, 7, 5],
+   [2, 8, 4]
+];
+
+console.log(spiralTraversal(matrix));
